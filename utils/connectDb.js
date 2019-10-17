84080d78fa04d09ef587ea3938b7a91f76c32027
cacheUp <bradb26@gmail.com>
2019-10-17 12:09:54 +0300

    mongo connect

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,1 +1,20 @@
-//
+import mongoose from "mongoose";
+
+const connection = {};
+
+async function connectDb() {
+  if (connection.isConnected) {
+    console.log("Using existing connection");
+    return;
+  }
+  const db = await mongoose.connect(process.env.MONG_SRV, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("database connection affirmative");
+  connection.isConnected = db.connections[0].readyState;
+}
+
+export default connectDb;
