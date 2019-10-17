15d84083d03afa1bc6c300f874aa9226562ec1a9
cacheUp <bradb26@gmail.com>
2019-10-17 13:02:53 +0300

    mongoose model

---
+++ models/Product.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import shortid from "shortid";
+
+const { String, Number } = mongoose.Schema.Types;
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  sku: {
+    type: String,
+    unique: true,
+    default: shortid.generate()
+  },
+  mediaUrl: {
+    type: String,
+    required: true
+  }
+});
