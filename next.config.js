cfb57691db28234e6604dfb9de2a6c3542652779
cacheUp <bradb26@gmail.com>
2019-10-17 11:32:21 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
+    MONGO_SRV:
+      "mongodb+srv://bradb26:Trophybox1@cluster0-rrbed.mongodb.net/portfolio-bball?retryWrites=true",
     JWT_SECRET: "<insert-jwt-secret>",
     CLOUDINARY_URL: "<insert-cloudinary-url>",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
