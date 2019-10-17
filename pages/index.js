c73fb31d972545e9e42b2263c8530c3de6698404
cacheUp <bradb26@gmail.com>
2019-10-17 11:20:13 +0300

    get initial props

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
-function Home() {
+import React, { useEffect } from "react";
+import axios from "axios";
+
+function Home({ products }) {
+  // useEffect(() => {
+  //   getProducts();
+  // }, []);
+
+  console.log(products);
+  // async function getProducts() {
+  //   const url = "http://localhost:3000/api/products";
+  //   const { data } = await axios.get(url);
+  //   console.log(data);
+  // }
+
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  const url = "http://localhost:3000/api/products";
+  const { data } = await axios.get(url);
+  return { products: data };
+};
 
 export default Home;
