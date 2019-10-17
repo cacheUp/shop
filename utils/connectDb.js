a45adee8944c1cd1a14bcd599a4e67c08c519b50
cacheUp <bradb26@gmail.com>
2019-10-17 13:46:09 +0300

    bug fix: data base

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -7,14 +7,20 @@ async function connectDb() {
     console.log("Using existing connection");
     return;
   }
-  const db = await mongoose.connect(process.env.MONGO_SRV, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  console.log("database connection affirmative");
-  connection.isConnected = db.connections[0].readyState;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_SRV, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log("database connection affirmative");
+    connection.isConnected = db.connections[0].readyState;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 export default connectDb;
