f2f25ecf2d2ca904ce1db03edf8431dac1573423
cacheUp <bradb26@gmail.com>
2019-10-18 08:21:50 +0300

    product data coming through

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -2,13 +2,13 @@ import { Card } from "semantic-ui-react";
 
 function ProductList({ products }) {
   const mapProductsToItems = products => {
-    return products.map(product => ({
+    return products.map((product, index) => ({
       header: product.name,
       image: product.mediaUrl,
       meta: `$${product.price}`,
       color: "teal",
       fluid: true,
-      childKey: product._id,
+      childKey: index,
       href: `/product?_id=${product._id}`
     }));
   };
--- pages/product.js
+++ pages/product.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-function Product(props) {
+function Product({ product }) {
+  console.log(product);
   return <>product</>;
 }
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  const url = `http//localhost:3000/api/product`;
+  const url = `http://localhost:3000/api/product`;
   const payload = { params: { _id } };
   const { data } = await axios.get(url, payload);
+  return { product: data };
 };
 
 export default Product;
