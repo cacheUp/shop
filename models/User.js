a1d80336fdf26e94167fd16d3f357c4997714bf2
cacheUp <bradb26@gmail.com>
2019-10-18 15:48:33 +0300

    finished user model

--- models/User.js
+++ models/User.js
@@ -2,24 +2,30 @@ import mongoose from "mongoose";
 
 const { String } = mongoose.Schema.Types;
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false
+    },
+    role: {
+      type: String,
+      required: true,
+      default: "user",
+      enum: ["user", "admin", "root"]
+    }
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false
-  },
-  role: {
-    type: String,
-    required: true,
-    default: "user"
-  }
-});
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
