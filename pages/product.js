77f0a2095e25379c39d73b29cad5006c1fda2ed4
cacheUp <bradb26@gmail.com>
2019-10-18 08:07:20 +0300

    G.I.P on product page

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,13 @@
-function Product() {
+import axios from "axios";
+
+function Product(props) {
   return <>product</>;
 }
+
+Product.getInitialProps = async ({ query: { _id } }) => {
+  const url = `http//localhost:3000/api/product`;
+  const payload = { params: { _id } };
+  const { data } = await axios.get(url, payload);
+};
 
 export default Product;
