cb53367d0c6357714625be97db0b1d058f50943e
cacheUp <bradb26@gmail.com>
2019-10-22 10:23:04 +0300

    put request to cart

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 import { Input } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import axios from "axios";
+import baseUrl from "../../utils/baseUrl";
+import cookie from "js-cookie";
 
-function AddProductToCart({ user }) {
+function AddProductToCart({ user, productId }) {
   const [quantity, setQuantity] = useState(1);
+  const router = useRouter();
+
+  async function handleAddProductToCart() {
+    const url = `${baseUrl}/api/cart`;
+    const payload = { quantity, productId };
+    const token = cookie.get("token");
+    const headers = { headers: { Authorization: token } };
+
+    await axios.put(url, payload, headers);
+  }
 
   return (
     <Input
@@ -13,8 +27,18 @@ function AddProductToCart({ user }) {
       placeholder="Quantity"
       action={
         user
-          ? { color: "orange", content: "Add to Cart", icon: "plus cart" }
-          : { color: "blue", content: "Sign Up To Purchase", icon: "signup" }
+          ? {
+              color: "orange",
+              content: "Add to Cart",
+              icon: "plus cart",
+              onClick: () => handleAddProductToCart
+            }
+          : {
+              color: "blue",
+              content: "Sign Up To Purchase",
+              icon: "signup",
+              onClick: () => router.push("/signup")
+            }
       }
     />
   );
