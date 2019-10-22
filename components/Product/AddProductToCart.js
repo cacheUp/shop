e3b4e228ffc3ab3942d15d3308f35ff856a9c69d
cacheUp <bradb26@gmail.com>
2019-10-22 08:35:24 +0300

    increment and decrement on product page

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,11 +1,15 @@
+import React, { useState } from "react";
 import { Input } from "semantic-ui-react";
 
 function AddProductToCart() {
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <Input
       type="number"
       min="1"
-      value="1"
+      value={quantity}
+      onChange={event => setQuantity(Number(event.target.value))}
       placeholder="Quantity"
       action={{ color: "orange", content: "Add to Cart", icon: "plus cart" }}
     />
--- pages/cart.js
+++ pages/cart.js
@@ -5,7 +5,8 @@ import { parseCookies } from "nookies";
 import axios from "axios";
 import baseUrl from "../utils/baseUrl";
 
-function Cart() {
+function Cart({ products }) {
+  console.log(products);
   return (
     <Segment>
       <CartItemList />
@@ -22,6 +23,8 @@ Cart.getInitialProps = async ctx => {
   const url = `${baseUrl}/api/cart`;
   const payload = { headers: { Authorization: token } };
   const { data } = await axios.get(url, payload);
+  console.log({ data });
+  return { products: data };
 };
 
 export default Cart;
