a287dc98b3e2d50ed6195bbefed0955a26a199c8
cacheUp <bradb26@gmail.com>
2019-10-22 11:49:29 +0300

    add to cart

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -4,20 +4,35 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import baseUrl from "../../utils/baseUrl";
 import cookie from "js-cookie";
+import catchErrors from "../../utils/catchErrors";
 
 function AddProductToCart({ user, productId }) {
   const [quantity, setQuantity] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
   const router = useRouter();
 
   async function handleAddProductToCart() {
-    const url = `${baseUrl}/api/cart`;
-    const payload = { quantity, productId };
-    const token = cookie.get("token");
-    const headers = { headers: { Authorization: token } };
+    try {
+      setLoading(true);
+      console.log("im here");
+      const url = `${baseUrl}/api/cart`;
+      const payload = { quantity, productId };
+      const token = cookie.get("token");
+      const headers = { headers: { Authorization: token } };
 
-    await axios.put(url, payload, headers);
+      const { data } = await axios.put(url, payload, headers);
+      console.log(data);
+
+      setSuccess(true);
+    } catch (err) {
+      catchErrors(err, window.alert);
+    } finally {
+      setLoading(false);
+      setSuccess(false);
+    }
   }
-
+  console.log(user);
   return (
     <Input
       type="number"
@@ -26,12 +41,21 @@ function AddProductToCart({ user, productId }) {
       onChange={event => setQuantity(Number(event.target.value))}
       placeholder="Quantity"
       action={
-        user
+        user && success
+          ? {
+              color: "blue",
+              content: "Item Added!",
+              icon: "plus cart",
+              disabled: true
+            }
+          : user
           ? {
               color: "orange",
               content: "Add to Cart",
               icon: "plus cart",
-              onClick: () => handleAddProductToCart
+              loading,
+              disabled: loading,
+              onClick: () => handleAddProductToCart()
             }
           : {
               color: "blue",
